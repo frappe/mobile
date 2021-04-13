936cb0eb3ae7360881334a5aae7a647db7664cd3
Sumit Bhanushali <sumit.bhanushali63@gmail.com>
2021-04-13 17:11:39 +0530

    added custom bottom navigation bar

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -9,14 +9,31 @@ import 'package:frappe_app/views/profile_view.dart';
 import 'package:frappe_app/widgets/user_avatar.dart';
 import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  final PersistentTabController _controller =
+      PersistentTabController(initialIndex: 0);
+
   @override
   Widget build(BuildContext context) {
-    return PersistentTabView(
+    return PersistentTabView.custom(
       context,
+      itemCount: 3,
+      controller: _controller,
       screens: _buildScreens(),
-      items: _navBarsItems(),
-      navBarStyle: NavBarStyle.style12,
+      customWidget: CustomNavBarWidget(
+        items: _navBarsItems(),
+        onItemSelected: (index) {
+          setState(() {
+            _controller.index = index;
+          });
+        },
+        selectedIndex: _controller.index,
+      ),
     );
   }
 
@@ -32,25 +49,122 @@ class HomeView extends StatelessWidget {
     return [
       PersistentBottomNavBarItem(
         title: 'Home',
-        inactiveIcon: FrappeIcon(FrappeIcons.home_outlined),
-        icon: FrappeIcon(FrappeIcons.home_filled),
+        inactiveIcon: FrappeIcon(
+          FrappeIcons.home_outlined,
+          color: FrappePalette.grey[500],
+        ),
+        icon: FrappeIcon(
+          FrappeIcons.home_filled,
+        ),
         activeColorPrimary: FrappePalette.grey[800],
-        inactiveColorPrimary: FrappePalette.grey[800],
+        inactiveColorPrimary: FrappePalette.grey[500],
       ),
       PersistentBottomNavBarItem(
         title: 'Search',
-        icon: FrappeIcon(FrappeIcons.search),
+        icon: FrappeIcon(
+          FrappeIcons.search,
+          color: FrappePalette.grey[800],
+        ),
+        inactiveIcon: FrappeIcon(
+          FrappeIcons.search,
+          color: FrappePalette.grey[500],
+        ),
         activeColorPrimary: FrappePalette.grey[800],
-        inactiveColorPrimary: FrappePalette.grey[800],
+        inactiveColorPrimary: FrappePalette.grey[500],
       ),
       PersistentBottomNavBarItem(
         title: 'Profile',
         icon: UserAvatar(
           uid: Config().userId,
+          size: 12,
         ),
         activeColorPrimary: FrappePalette.grey[800],
-        inactiveColorPrimary: FrappePalette.grey[800],
+        inactiveColorPrimary: FrappePalette.grey[500],
       ),
     ];
+  }
+}
+
+class CustomNavBarWidget extends StatelessWidget {
+  final int selectedIndex;
+  final List<PersistentBottomNavBarItem> items;
+  final ValueChanged<int> onItemSelected;
+
+  CustomNavBarWidget({
+    Key key,
+    this.selectedIndex,
+    @required this.items,
+    this.onItemSelected,
+  });
+
+  Widget _buildItem(PersistentBottomNavBarItem item, bool isSelected) {
+    return Container(
+      alignment: Alignment.center,
+      height: kBottomNavigationBarHeight,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Flexible(
+            child: IconTheme(
+              data: IconThemeData(
+                  size: 26.0,
+                  color: isSelected
+                      ? (item.activeColorSecondary == null
+                          ? item.activeColorPrimary
+                          : item.activeColorSecondary)
+                      : item.inactiveColorPrimary == null
+                          ? item.activeColorPrimary
+                          : item.inactiveColorPrimary),
+              child: isSelected ? item.icon : item.inactiveIcon ?? item.icon,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 5.0),
+            child: Material(
+              type: MaterialType.transparency,
+              child: FittedBox(
+                  child: Text(
+                item.title,
+                style: TextStyle(
+                    color: isSelected
+                        ? (item.activeColorSecondary == null
+                            ? item.activeColorPrimary
+                            : item.activeColorSecondary)
+                        : item.inactiveColorPrimary,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 12.0),
+              )),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Container(
+        width: double.infinity,
+        height: kBottomNavigationBarHeight,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: items.map((item) {
+            int index = items.indexOf(item);
+            return FlatButton(
+              highlightColor: Colors.transparent,
+              splashColor: Colors.transparent,
+              onPressed: () {
+                this.onItemSelected(index);
+              },
+              child: _buildItem(item, selectedIndex == index),
+            );
+          }).toList(),
+        ),
+      ),
+    );
   }
 }
