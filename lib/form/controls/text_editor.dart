cbc6a2e6cf009bf7f9eed7b1c14b023372f07469
Sumit Bhanushali <sumit.bhanushali63@gmail.com>
2021-04-21 19:12:54 +0530

    added readonly text editor

--- lib/form/controls/text_editor.dart
+++ lib/form/controls/text_editor.dart
@@ -1,6 +1,10 @@
 // @dart=2.9
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:flutter_html/image_render.dart';
+import 'package:frappe_app/model/config.dart';
 import 'package:frappe_app/widgets/form_builder_text_editor.dart';
+// import 'package:html_editor_enhanced/html_editor.dart';
 
 import '../../config/palette.dart';
 import '../../model/doctype_response.dart';
@@ -19,7 +23,7 @@ class TextEditor extends StatelessWidget with Control, ControlInput {
   const TextEditor({
     this.key,
     @required this.doctypeField,
-    this.readOnly,
+    this.readOnly = false,
     this.doc,
     this.withLabel,
   });
@@ -28,8 +32,7 @@ class TextEditor extends StatelessWidget with Control, ControlInput {
   Widget build(BuildContext context) {
     List<String Function(dynamic)> validators = [];
 
-    String Function(dynamic) Function(BuildContext, {String errorText}) f =
-        setMandatory(doctypeField);
+    var f = setMandatory(doctypeField);
 
     if (f != null) {
       validators.add(
@@ -38,38 +41,48 @@ class TextEditor extends StatelessWidget with Control, ControlInput {
     }
     return FormBuilderTextEditor(
       onTap: (field) async {
-        var v = await Navigator.of(context).push(
+        Navigator.of(context).push(
           MaterialPageRoute(
             builder: (context) {
-              var _controller = TextEditingController();
-              return Scaffold(
-                appBar: AppBar(
-                  actions: [
-                    FlatButton(
-                      child: Text('save'),
-                      onPressed: () {
-                        Navigator.of(context).pop(_controller.text);
-                      },
-                    ),
-                  ],
-                ),
-                body: TextField(
-                  controller: _controller,
-                ),
+              return EditText(
+                data: doc != null ? doc[doctypeField.fieldname] : null,
               );
             },
           ),
         );
-
-        if (v != null) {
-          field.didChange(v);
-        }
       },
       key: key,
       initialValue: doc != null ? doc[doctypeField.fieldname] : null,
       name: doctypeField.fieldname,
       enabled: !readOnly,
       // validator: FormBuilderValidators.compose(validators),
+    );
+  }
+}
+
+class EditText extends StatelessWidget {
+  final String data;
+  const EditText({
+    Key key,
+    this.data,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Html(
+        onImageError: (e, ee) {
+          print("e $e");
+          print("ee $ee");
+        },
+        data: data,
+        customImageRenders: {
+          (attr, _) => attr["src"] != null: networkImageRender(mapUrl: (url) {
+            return Config().baseUrl + url;
+          }),
+        },
+      ),
     );
   }
 }
--- lib/widgets/form_builder_text_editor.dart
+++ lib/widgets/form_builder_text_editor.dart
@@ -2,8 +2,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:frappe_app/config/palette.dart';
+import 'package:frappe_app/model/config.dart';
+import 'package:html/parser.dart';
 
-/// Field for Dropdown button
 class FormBuilderTextEditor<T> extends FormBuilderField<T> {
   final Function onTap;
 
@@ -14,7 +17,7 @@ class FormBuilderTextEditor<T> extends FormBuilderField<T> {
     FormFieldValidator<T> validator,
     T initialValue,
     bool enabled = true,
-  }) : /*: assert(allowClear == true || clearIcon != null)*/ super(
+  }) : super(
           key: key,
           initialValue: initialValue,
           name: name,
@@ -22,25 +25,41 @@ class FormBuilderTextEditor<T> extends FormBuilderField<T> {
           builder: (FormFieldState<T> field) {
             final state = field as _FormBuilderDropdownState<T>;
 
+            var document = parse(field.value);
+            var imgs = document.getElementsByTagName('img');
+
+            imgs.forEach((img) {
+              if (Uri.parse(img.attributes["src"]).hasAbsolutePath) {
+                img.attributes["src"] =
+                    "${Config().baseUrl}${img.attributes["src"]}";
+              }
+            });
+
             void changeValue(T value) {
               state.requestFocus();
               state.didChange(value);
             }
 
             return InkWell(
-              onTap: !enabled
-                  ? null
-                  : () async {
-                      onTap(field);
-                    },
+              // onTap: !enabled
+              //     ? null
+              //     : () async {
+              //         onTap(field);
+              //       },
+              onTap: () {
+                onTap(field);
+              },
               child: ConstrainedBox(
                 constraints: BoxConstraints(
                   maxHeight: 80,
                   minWidth: double.infinity,
                 ),
-                child: SingleChildScrollView(
-                  child: Text(
-                    field.value.toString(),
+                child: Container(
+                  color: Palette.fieldBgColor,
+                  child: SingleChildScrollView(
+                    child: Html(
+                      data: document.outerHtml,
+                    ),
                   ),
                 ),
               ),
