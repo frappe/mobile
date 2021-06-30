a860d5c0729e3f4f9a9f8d9d6b900254fa5ab527
Sumit Bhanushali <sumit.bhanushali63@gmail.com>
2021-06-30 17:18:04 +0530

    view email redesign

--- lib/config/frappe_icons.dart
+++ lib/config/frappe_icons.dart
@@ -24,6 +24,7 @@ class FrappeIcons {
   static const frappe = '$_basePath/frappe_ico.svg';
   static const add_user = '$_basePath/add_user.svg';
   static const down_arrow = '$_basePath/down_arrow.svg';
+  static const up_arrow = '$_basePath/up_arrow.svg';
   static const close_alt = '$_basePath/close_alt.svg';
   static const image_add = '$_basePath/image_add.svg';
   static const small_file = '$_basePath/small_file.svg';
@@ -34,4 +35,12 @@ class FrappeIcons {
   static const arrow_right = '$_basePath/arrow_right.svg';
   static const email = '$_basePath/email.svg';
   static const unlock = '$_basePath/unlock.svg';
+  static const sort_ascending = '$_basePath/sort_ascending.svg';
+  static const sort_descending = '$_basePath/sort_descending.svg';
+  static const tick = '$_basePath/tick.svg';
+  static const reply_all = '$_basePath/reply_all.svg';
+  static const reply_alt = '$_basePath/reply_alt.svg';
+  static const dot_vertical = '$_basePath/dot_vertical.svg';
+  static const read_status = '$_basePath/read_status.svg';
+  static const unread_status = '$_basePath/unread_status.svg';
 }
--- lib/widgets/email_box.dart
+++ lib/widgets/email_box.dart
@@ -2,11 +2,13 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
+import 'package:frappe_app/config/frappe_icons.dart';
+import 'package:frappe_app/config/frappe_palette.dart';
 import 'package:frappe_app/model/get_doc_response.dart';
 import 'package:frappe_app/utils/dio_helper.dart';
+import 'package:frappe_app/utils/frappe_icon.dart';
 import 'package:frappe_app/views/login/login_view.dart';
 
-import 'package:html/parser.dart';
 import 'package:timeago/timeago.dart' as timeago;
 import 'package:url_launcher/url_launcher.dart';
 
@@ -17,63 +19,213 @@ import '../model/config.dart';
 import '../widgets/user_avatar.dart';
 import 'package:photo_view/photo_view.dart';
 
-class EmailBox extends StatelessWidget {
+class EmailBox extends StatefulWidget {
   final Communication data;
+  final Function onReplyTo;
+  final Function onReplyAll;
 
-  EmailBox(this.data);
+  EmailBox({
+    required this.data,
+    required this.onReplyAll,
+    required this.onReplyTo,
+  });
+
+  @override
+  _EmailBoxState createState() => _EmailBoxState();
+}
+
+class _EmailBoxState extends State<EmailBox> {
+  late bool _isExpanded;
+
+  @override
+  void initState() {
+    _isExpanded = false;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    var time = timeago.format(DateTime.parse(data.creation));
+    var time = timeago.format(DateTime.parse(widget.data.creation));
 
-    var document = parse(data.content);
-    String parsedContent = parse(document.body!.text).documentElement!.text;
-
-    return Container(
-      color: Colors.white,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8.0),
-        child: ListTile(
-          onTap: () {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) {
-                  return ViewEmail(
-                    time: time,
-                    title: data.subject,
-                    senderFullName: data.senderFullName,
-                    sender: data.sender,
-                    content: data.content,
-                  );
-                },
-              ),
-            );
+    return GestureDetector(
+      onTap: () {
+        setState(
+          () {
+            _isExpanded = !_isExpanded;
           },
-          subtitle:
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            Container(
-              padding: const EdgeInsets.only(bottom: 8.0, top: 4.0),
-              child: Text(
-                data.subject,
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-                style: TextStyle(color: Colors.black),
-              ),
-            ),
-            Text(
-              parsedContent,
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-            )
-          ]),
-          title: Row(
+        );
+      },
+      child: ConstrainedBox(
+        constraints: BoxConstraints(
+          maxHeight: _isExpanded ? double.infinity : 184,
+        ),
+        child: Card(
+          color: Colors.white,
+          child: Column(
             children: [
-              Text('${data.senderFullName}'),
-              Spacer(),
-              Text(
-                time,
-                style: TextStyle(
-                  fontSize: 12,
+              ListTile(
+                leading: UserAvatar(
+                  uid: widget.data.sender,
+                ),
+                title: Row(
+                  children: [
+                    Flexible(
+                      child: Text(
+                        widget.data.senderFullName,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    if (widget.data.deliveryStatus != "")
+                      widget.data.deliveryStatus == "Sent"
+                          ? FrappeIcon(
+                              FrappeIcons.unread_status,
+                            )
+                          : FrappeIcon(
+                              FrappeIcons.read_status,
+                              color: FrappePalette.blue,
+                            ),
+                  ],
+                ),
+                subtitle: Text(
+                  time,
+                ),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    // FrappeIcon(
+                    //   FrappeIcons.favourite_resting,
+                    //   color: FrappePalette.grey,
+                    //   size: 20,
+                    // ),
+                    // SizedBox(
+                    //   width: 11,
+                    // ),
+                    GestureDetector(
+                      onTap: () {
+                        widget.onReplyTo();
+                      },
+                      child: FrappeIcon(
+                        FrappeIcons.reply_alt,
+                        color: FrappePalette.grey,
+                        size: 18,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 11,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        widget.onReplyAll();
+                      },
+                      child: FrappeIcon(
+                        FrappeIcons.reply_all,
+                      ),
+                    ),
+                    // SizedBox(
+                    //   width: 11,
+                    // ),
+                    // FrappeIcon(
+                    //   FrappeIcons.dot_vertical,
+                    // ),
+                  ],
+                ),
+              ),
+              Flexible(
+                child: ShaderMask(
+                  shaderCallback: (rect) {
+                    return LinearGradient(
+                      begin: Alignment(0.0, 0.6),
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        Colors.white,
+                        _isExpanded ? Colors.white : Colors.white10
+                      ],
+                    ).createShader(
+                        Rect.fromLTRB(0, 0, rect.width, rect.height));
+                  },
+                  child: Container(
+                    color: Colors.white,
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Html(
+                      data: widget.data.content,
+                      customRender: {
+                        "img": (renderContext, child) {
+                          var src = renderContext.tree.attributes['src'];
+                          if (src != null) {
+                            if (!src.startsWith("http")) {
+                              src = Config().baseUrl! + src;
+                            }
+                            return GestureDetector(
+                              onTap: () => Navigator.of(
+                                context,
+                                rootNavigator: true,
+                              ).push(
+                                MaterialPageRoute(
+                                  builder: (context) => Scaffold(
+                                    appBar: AppBar(
+                                      elevation: 0.8,
+                                    ),
+                                    body: PhotoView(
+                                      imageProvider: NetworkImage(
+                                        src!,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              child: Image.network(
+                                src,
+                                headers: {
+                                  HttpHeaders.cookieHeader: DioHelper.cookies!,
+                                },
+                              ),
+                            );
+                          }
+                        },
+                      },
+                      customImageRenders: {
+                        networkSourceMatcher(domains: [
+                          Config().baseUrl!,
+                        ]): networkImageRender(
+                          headers: {
+                            HttpHeaders.cookieHeader: DioHelper.cookies!,
+                          },
+                          altWidget: (alt) => Text(alt ?? ""),
+                          loadingWidget: () => Text("Loading..."),
+                        ),
+                        // for relative paths, prefix with a base url
+                        (attr, _) =>
+                                attr["src"] != null &&
+                                !attr["src"]!.startsWith("http"):
+                            networkImageRender(
+                          headers: {
+                            HttpHeaders.cookieHeader: DioHelper.cookies!,
+                          },
+                          mapUrl: (url) => Config().baseUrl! + url!,
+                        ),
+                        // Custom placeholder image for broken links
+                        networkSourceMatcher():
+                            networkImageRender(altWidget: (_) => FrappeLogo()),
+                      },
+                      onLinkTap: (url, _, __, ___) async {
+                        print("Opening $url...");
+                        if (url != null) {
+                          if (await canLaunch(url)) {
+                            await launch(
+                              url,
+                              headers: {
+                                HttpHeaders.cookieHeader: DioHelper.cookies!,
+                              },
+                            );
+                          } else {
+                            throw 'Could not launch $url';
+                          }
+                        }
+                      },
+                    ),
+                  ),
                 ),
               )
             ],
@@ -119,7 +271,9 @@ class ViewEmail extends StatelessWidget {
                       child: Text(
                         title,
                         style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 18),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
                       ),
                     ),
                     ListTile(
