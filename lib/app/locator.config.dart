604f403d7227d3e109b1709fe96acfec3e790d49
Sumit Bhanushali <sumit.bhanushali63@gmail.com>
2021-07-23 12:33:08 +0530

    singleton for custom form

--- lib/app/locator.config.dart
+++ lib/app/locator.config.dart
@@ -8,34 +8,35 @@ import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
 import '../services/connectivity_service.dart' as _i7;
-import '../services/storage_service.dart' as _i17;
+import '../services/storage_service.dart' as _i18;
 import '../views/awesome_bar/awesome_bar_viewmodel.dart' as _i6;
-import '../views/desk/desk_viewmodel.dart' as _i8;
+import '../views/desk/desk_viewmodel.dart' as _i9;
 import '../views/form_view/bottom_sheets/assignees/assignees_bottom_sheet_viewmodel.dart'
     as _i5;
 import '../views/form_view/bottom_sheets/attachments/add_attachments_bottom_sheet_viewmodel.dart'
     as _i3;
 import '../views/form_view/bottom_sheets/attachments/view_attachments_bottom_sheet_viewmodel.dart'
-    as _i19;
+    as _i20;
 import '../views/form_view/bottom_sheets/reviews/add_review_bottom_sheet_viewmodel.dart'
     as _i4;
 import '../views/form_view/bottom_sheets/reviews/view_reviews_bottom_sheet_viewmodel.dart'
-    as _i20;
+    as _i21;
 import '../views/form_view/bottom_sheets/share/share_bottom_sheet_viewmodel.dart'
-    as _i15;
-import '../views/form_view/bottom_sheets/tags/tags_bottom_sheet_viewmodel.dart'
-    as _i18;
-import '../views/form_view/form_view_viewmodel.dart' as _i11;
-import '../views/list_view/bottom_sheets/edit_filter_bottom_sheet_viewmodel.dart'
-    as _i9;
-import '../views/list_view/bottom_sheets/filters_bottom_sheet_viewmodel.dart'
-    as _i10;
-import '../views/list_view/bottom_sheets/sort_by_fields_bottom_sheet_viewmodel.dart'
     as _i16;
-import '../views/list_view/list_view_viewmodel.dart' as _i12;
-import '../views/login/login_viewmodel.dart' as _i13;
-import '../views/new_doc/new_doc_viewmodel.dart'
-    as _i14; // ignore_for_file: unnecessary_lambdas
+import '../views/form_view/bottom_sheets/tags/tags_bottom_sheet_viewmodel.dart'
+    as _i19;
+import '../views/form_view/form_view_viewmodel.dart' as _i12;
+import '../views/list_view/bottom_sheets/edit_filter_bottom_sheet_viewmodel.dart'
+    as _i10;
+import '../views/list_view/bottom_sheets/filters_bottom_sheet_viewmodel.dart'
+    as _i11;
+import '../views/list_view/bottom_sheets/sort_by_fields_bottom_sheet_viewmodel.dart'
+    as _i17;
+import '../views/list_view/list_view_viewmodel.dart' as _i13;
+import '../views/login/login_viewmodel.dart' as _i14;
+import '../views/new_doc/new_doc_viewmodel.dart' as _i15;
+import '../widgets/custom_form.dart'
+    as _i8; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
@@ -50,25 +51,26 @@ _i1.GetIt $initGetIt(_i1.GetIt get,
       () => _i5.AssigneesBottomSheetViewModel());
   gh.lazySingleton<_i6.AwesomBarViewModel>(() => _i6.AwesomBarViewModel());
   gh.lazySingleton<_i7.ConnectivityService>(() => _i7.ConnectivityService());
-  gh.lazySingleton<_i8.DeskViewModel>(() => _i8.DeskViewModel());
-  gh.lazySingleton<_i9.EditFilterBottomSheetViewModel>(
-      () => _i9.EditFilterBottomSheetViewModel());
-  gh.lazySingleton<_i10.FiltersBottomSheetViewModel>(
-      () => _i10.FiltersBottomSheetViewModel());
-  gh.lazySingleton<_i11.FormViewViewModel>(() => _i11.FormViewViewModel());
-  gh.lazySingleton<_i12.ListViewViewModel>(() => _i12.ListViewViewModel());
-  gh.lazySingleton<_i13.LoginViewModel>(() => _i13.LoginViewModel());
-  gh.lazySingleton<_i14.NewDocViewModel>(() => _i14.NewDocViewModel());
-  gh.lazySingleton<_i15.ShareBottomSheetViewModel>(
-      () => _i15.ShareBottomSheetViewModel());
-  gh.lazySingleton<_i16.SortByFieldsBottomSheetViewModel>(
-      () => _i16.SortByFieldsBottomSheetViewModel());
-  gh.lazySingleton<_i17.StorageService>(() => _i17.StorageService());
-  gh.lazySingleton<_i18.TagsBottomSheetViewModel>(
-      () => _i18.TagsBottomSheetViewModel());
-  gh.lazySingleton<_i19.ViewAttachmenetsBottomSheetViewModel>(
-      () => _i19.ViewAttachmenetsBottomSheetViewModel());
-  gh.lazySingleton<_i20.ViewReviewsBottomSheetViewModel>(
-      () => _i20.ViewReviewsBottomSheetViewModel());
+  gh.lazySingleton<_i8.CustomFormViewModel>(() => _i8.CustomFormViewModel());
+  gh.lazySingleton<_i9.DeskViewModel>(() => _i9.DeskViewModel());
+  gh.lazySingleton<_i10.EditFilterBottomSheetViewModel>(
+      () => _i10.EditFilterBottomSheetViewModel());
+  gh.lazySingleton<_i11.FiltersBottomSheetViewModel>(
+      () => _i11.FiltersBottomSheetViewModel());
+  gh.lazySingleton<_i12.FormViewViewModel>(() => _i12.FormViewViewModel());
+  gh.lazySingleton<_i13.ListViewViewModel>(() => _i13.ListViewViewModel());
+  gh.lazySingleton<_i14.LoginViewModel>(() => _i14.LoginViewModel());
+  gh.lazySingleton<_i15.NewDocViewModel>(() => _i15.NewDocViewModel());
+  gh.lazySingleton<_i16.ShareBottomSheetViewModel>(
+      () => _i16.ShareBottomSheetViewModel());
+  gh.lazySingleton<_i17.SortByFieldsBottomSheetViewModel>(
+      () => _i17.SortByFieldsBottomSheetViewModel());
+  gh.lazySingleton<_i18.StorageService>(() => _i18.StorageService());
+  gh.lazySingleton<_i19.TagsBottomSheetViewModel>(
+      () => _i19.TagsBottomSheetViewModel());
+  gh.lazySingleton<_i20.ViewAttachmenetsBottomSheetViewModel>(
+      () => _i20.ViewAttachmenetsBottomSheetViewModel());
+  gh.lazySingleton<_i21.ViewReviewsBottomSheetViewModel>(
+      () => _i21.ViewReviewsBottomSheetViewModel());
   return get;
 }
