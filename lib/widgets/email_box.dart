4c536aa04848379225a41eb26b6f982ac4ef5112
Sumit Bhanushali <sumit.bhanushali63@gmail.com>
2021-07-30 15:03:49 +0530

    show/download attachments from email

--- lib/widgets/email_box.dart
+++ lib/widgets/email_box.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
@@ -7,12 +8,12 @@ import 'package:frappe_app/config/frappe_palette.dart';
 import 'package:frappe_app/model/get_doc_response.dart';
 import 'package:frappe_app/utils/dio_helper.dart';
 import 'package:frappe_app/utils/frappe_icon.dart';
+import 'package:frappe_app/utils/helpers.dart';
 import 'package:frappe_app/views/login/login_view.dart';
+import 'package:open_file/open_file.dart';
 
 import 'package:timeago/timeago.dart' as timeago;
 import 'package:url_launcher/url_launcher.dart';
-
-import '../config/palette.dart';
 
 import '../model/config.dart';
 
@@ -45,7 +46,16 @@ class _EmailBoxState extends State<EmailBox> {
 
   @override
   Widget build(BuildContext context) {
+    var message = widget.data.content;
+
     var time = timeago.format(DateTime.parse(widget.data.creation));
+    var attachments = (json.decode(widget.data.attachments) as List)
+        .map(
+          (attachment) => Attachments.fromJson(
+            attachment,
+          ),
+        )
+        .toList();
 
     return GestureDetector(
       onTap: () {
@@ -61,7 +71,11 @@ class _EmailBoxState extends State<EmailBox> {
         ),
         child: Card(
           color: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               ListTile(
                 leading: UserAvatar(
@@ -132,6 +146,93 @@ class _EmailBoxState extends State<EmailBox> {
                   ],
                 ),
               ),
+              if (attachments.isNotEmpty && _isExpanded)
+                Container(
+                  height: 50,
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: attachments.length,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      var fileName = attachments[index].fileUrl.split("/").last;
+                      var extension = fileName.split('.').last;
+
+                      return InkWell(
+                        onTap: () async {
+                          var downloadPath = await getDownloadPath();
+
+                          var filePath = "$downloadPath$fileName";
+
+                          var fileExists = await File(filePath).exists();
+
+                          if (fileExists) {
+                            OpenFile.open(filePath);
+                          } else {
+                            downloadFile(
+                                attachments[index].fileUrl, downloadPath);
+                          }
+                        },
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 12,
+                          ),
+                          child: Container(
+                            width: 162,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              border:
+                                  Border.all(color: FrappePalette.grey[200]!),
+                            ),
+                            child: Row(
+                              children: [
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                CircleAvatar(
+                                  backgroundColor: FrappePalette.grey[100],
+                                  child: FrappeIcon(
+                                    FrappeIcons.small_file,
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 8,
+                                ),
+                                Flexible(
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        fileName,
+                                        overflow: TextOverflow.ellipsis,
+                                        style: TextStyle(
+                                          color: FrappePalette.grey[900],
+                                          fontSize: 13,
+                                        ),
+                                      ),
+                                      Text(
+                                        extension.toUpperCase(),
+                                        style: TextStyle(
+                                          color: FrappePalette.grey[600],
+                                          fontWeight: FontWeight.w400,
+                                          fontSize: 12,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
               Flexible(
                 child: ShaderMask(
                   shaderCallback: (rect) {
@@ -143,13 +244,19 @@ class _EmailBoxState extends State<EmailBox> {
                         _isExpanded ? Colors.white : Colors.white10
                       ],
                     ).createShader(
-                        Rect.fromLTRB(0, 0, rect.width, rect.height));
+                      Rect.fromLTRB(
+                        0,
+                        0,
+                        rect.width,
+                        rect.height,
+                      ),
+                    );
                   },
                   child: Container(
                     color: Colors.white,
                     padding: const EdgeInsets.symmetric(horizontal: 8.0),
                     child: Html(
-                      data: widget.data.content,
+                      data: message,
                       customRender: {
                         "img": (renderContext, child) {
                           var src = renderContext.tree.attributes['src'];
@@ -230,144 +337,6 @@ class _EmailBoxState extends State<EmailBox> {
               )
             ],
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class ViewEmail extends StatelessWidget {
-  final String title;
-  final String time;
-  final String senderFullName;
-  final String sender;
-  final String content;
-
-  ViewEmail({
-    required this.title,
-    required this.time,
-    required this.senderFullName,
-    required this.sender,
-    required this.content,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Card(
-              elevation: 4,
-              child: Container(
-                color: Palette.bgColor,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        title,
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18,
-                        ),
-                      ),
-                    ),
-                    ListTile(
-                      leading: UserAvatar(
-                        uid: sender,
-                      ),
-                      title: Text(senderFullName),
-                      subtitle: Text(time),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Container(
-              color: Colors.white,
-              padding: const EdgeInsets.all(8.0),
-              child: Html(
-                data: content,
-                customRender: {
-                  "img": (renderContext, child) {
-                    var src = renderContext.tree.attributes['src'];
-                    if (src != null) {
-                      if (!src.startsWith("http")) {
-                        src = Config().baseUrl! + src;
-                      }
-                      return GestureDetector(
-                        onTap: () => Navigator.of(
-                          context,
-                          rootNavigator: true,
-                        ).push(
-                          MaterialPageRoute(
-                            builder: (context) => Scaffold(
-                              appBar: AppBar(
-                                elevation: 0.8,
-                              ),
-                              body: PhotoView(
-                                imageProvider: NetworkImage(
-                                  src!,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                        child: Image.network(
-                          src,
-                          headers: {
-                            HttpHeaders.cookieHeader: DioHelper.cookies!,
-                          },
-                        ),
-                      );
-                    }
-                  },
-                },
-                customImageRenders: {
-                  networkSourceMatcher(domains: [
-                    Config().baseUrl!,
-                  ]): networkImageRender(
-                    headers: {
-                      HttpHeaders.cookieHeader: DioHelper.cookies!,
-                    },
-                    altWidget: (alt) => Text(alt ?? ""),
-                    loadingWidget: () => Text("Loading..."),
-                  ),
-                  // for relative paths, prefix with a base url
-                  (attr, _) =>
-                      attr["src"] != null &&
-                      !attr["src"]!.startsWith("http"): networkImageRender(
-                    headers: {
-                      HttpHeaders.cookieHeader: DioHelper.cookies!,
-                    },
-                    mapUrl: (url) => Config().baseUrl! + url!,
-                  ),
-                  // Custom placeholder image for broken links
-                  networkSourceMatcher():
-                      networkImageRender(altWidget: (_) => FrappeLogo()),
-                },
-                onLinkTap: (url, _, __, ___) async {
-                  print("Opening $url...");
-                  if (url != null) {
-                    if (await canLaunch(url)) {
-                      await launch(
-                        url,
-                        headers: {
-                          HttpHeaders.cookieHeader: DioHelper.cookies!,
-                        },
-                      );
-                    } else {
-                      throw 'Could not launch $url';
-                    }
-                  }
-                },
-              ),
-            )
-          ],
         ),
       ),
     );
