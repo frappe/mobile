2614bda66bfd9d864cae89527a209729700c453d
Sumit Bhanushali <sumit.bhanushali63@gmail.com>
2021-11-10 19:42:57 +0530

    fix: add form validator delegate

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_mentions/flutter_mentions.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:frappe_app/views/home_view.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
@@ -63,6 +64,9 @@ class _FrappeAppState extends State<FrappeApp> {
             debugShowCheckedModeBanner: false,
             title: 'Frappe',
             theme: theme,
+            localizationsDelegates: [
+              FormBuilderLocalizations.delegate,
+            ],
             home: GestureDetector(
               onTap: () {
                 FocusScope.of(context).requestFocus(new FocusNode());
